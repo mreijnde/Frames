7d5c30b673c8c9b9ff9fcad19f9ae4b8a64f115a
benjamingaudin <benjamin.gaudin@hotmail.com>
2021-05-31 18:53:34 +0200

    align

--- +Frames/align.m
+++ +Frames/align.m
@@ -1,5 +1,16 @@
-function frame = align(varargin)
+function varargout = align(dfs)
 %UNTITLED7 Summary of this function goes here
 %   Detailed explanation goes here
+arguments(Repeating)
+    dfs frames.DataFrame
+end
+warning('off','frames:Index:notUnique')
+concatenate = [dfs{:}];  % align
+warning('on','frames:Index:notUnique')
+colID = 1;
+for ii=1:nargin  % split into varargout
+    nbCols = length(dfs{ii}.columns);
+    varargout{ii} = concatenate.iloc(':',colID:colID+nbCols-1); %#ok<AGROW>
+    colID=colID+nbCols;
 end
 
