2c36bb266f8156037aa494b99ae03c56891744cf
benjamingaudin <benjamin.gaudin@hotmail.com>
2021-06-02 22:28:11 +0200

    better look

--- +Frames/private/transformIndex.m
+++ +Frames/private/transformIndex.m
@@ -3,11 +3,12 @@ arguments
     index  % only occurences are when index is a frames.Index
     type (1,1) {mustBeMember(type,["unsorted","sorted","time"])}
 end
-if strcmp(type,"unsorted")
-    index = frames.UniqueIndex(index);
-elseif strcmp(type,"sorted")
-    index = frames.SortedIndex(index);
-elseif strcmp(type,"time")
-    index = frames.TimeIndex(index);
+switch type
+    case "unsorted"
+        index = frames.UniqueIndex(index);
+    case "sorted"
+        index = frames.SortedIndex(index);
+    case "time"
+        index = frames.TimeIndex(index);
 end
 end
