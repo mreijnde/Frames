60a91ffc01db0c1d3948ecc2aae62463fa0f33e8
benjamingaudin <benjamin.gaudin@hotmail.com>
2021-06-02 19:56:20 +0200

    timeindex selectable with string  and default format is Matlab's

--- +Frames/TimeIndex.m
+++ +Frames/TimeIndex.m
@@ -1,6 +1,6 @@
 classdef TimeIndex < frames.SortedIndex
     properties
-        format {mustBeTextScalar} = "dd.MM.yyyy"
+        format {mustBeTextScalar} = "dd-MMM-yyyy"
     end
     %UNTITLED4 Summary of this class goes here
     %   Detailed explanation goes here
@@ -11,37 +11,66 @@ classdef TimeIndex < frames.SortedIndex
             arguments
                 value
                 nameValue.name = ""
-                nameValue.format = "dd.MM.yyyy"
+                nameValue.format = "dd-MMM-yyyy"
             end
             if isdatetime(value); nameValue.format = value.Format; end
             obj = obj@frames.SortedIndex(value,name=nameValue.name);
             obj.format = nameValue.format;
         end
-
-        function value = getValue(obj)
-            value = datetime(obj.value_,ConvertFrom='datenum',Format=obj.format);
-        end
-        function value_ = getValue_(obj,value_)
-            value_ = getValue_@frames.SortedIndex(obj,value_);
-            switch class(value_)
-                case 'datetime'
-                case {'string','cell'}
-                    value_ = datetime(value_,Format=obj.format);
-                case 'double'
-                    return
-                otherwise
-                    error('type of time index not recognized')
+        
+        function pos = positionOf(obj,selector)
+            if isTextScalar(selector) && contains(selector,':')
+                selector = obj.getTimerange(selector);
             end
-            value_ = datenum(value_);
+            if isa(selector,'timerange')
+                tt = timetable(obj.value);
+                [~,whichRows] = overlapsrange(tt,selector);
+                ids = (1:length(obj.value_))';
+                pos = ids(whichRows);
+                return
+            end
+            pos = positionOf@frames.SortedIndex(obj,selector);
         end
+        
+        
     end
     methods(Access = protected)
         function value = valueChecker(obj,value)
             if ~isunique(value) || ~issorted(value)
                 error('index is not unique and sorted')
             end
-            value = obj.getValue_(value);
+            value = obj.getValue_from(value);
+        end
+        
+        function value = getValue(obj)
+            value = datetime(obj.value_,ConvertFrom='datenum',Format=obj.format);
+        end
+        function value = getValue_from(obj,value)
+            value = getValue_from@frames.SortedIndex(obj,value);
+            switch class(value)
+                case 'datetime'
+                case {'string','cell'}
+                    value = datetime(value,Format=obj.format);
+                case 'double'
+                    return
+                otherwise
+                    error('type of time index not recognized')
+            end
+            value = datenum(value);
+        end
+        function selector = getTimerange(obj,selector)
+            splitted = split(selector,':');
+            if length(splitted)==3
+                format_ = splitted{3};
+                splitted = splitted(1:2);
+            elseif length(splitted)==2
+                format_ = obj.format;
+            else
+                error('timerange selection from string must be of type "dateStart:dateEnd[:format]"')
+            end
+            selector = timerangeFromString(splitted,format_);
         end
     end
     
 end
+
