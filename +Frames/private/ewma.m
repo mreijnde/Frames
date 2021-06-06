0f0306a60ffdca0c56530c414341ca66e7555778
benjamingaudin <benjamin.gaudin@hotmail.com>
2021-06-06 22:01:04 +0200

    ewma

---
+++ +Frames/private/ewma.m
@@ -0,0 +1,19 @@
+function y = ewma(data,alpha)
+%Compute ewma.
+% Formula:
+% y_0 = data_0
+% y_i = alpha .* data_i + (1-alpha) .* y_(i-1)
+% NaNs are ignored in the computation, ie they are removed before
+% computing.
+y = arrayfun(@(x) ewma_(data(:,x),alpha),(1:size(data,2)),UniformOutput=false);
+y = cell2mat(y);
+end
+
+function y = ewma_(data,alpha)
+isvalid = ~isnan(data);
+data = data(isvalid);
+ema = filter(alpha,[1,alpha-1],data(2:end),(1-alpha).*data(1));
+ema = [data(1);ema];
+y = NaN(size(isvalid));
+y(isvalid) = ema;
+end
