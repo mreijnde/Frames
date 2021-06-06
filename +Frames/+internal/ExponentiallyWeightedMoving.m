03bb0dbd86840ad542ea6f4efa242323baa35d01
Benjamin Gaudin <57812158+benjamingaudin@users.noreply.github.com>
2021-06-07 00:09:28 +0200

    Add comments

--- +Frames/+internal/ExponentiallyWeightedMoving.m
+++ +Frames/+internal/ExponentiallyWeightedMoving.m
@@ -23,6 +23,7 @@ classdef ExponentiallyWeightedMoving
             df = obj.df;
             df.data = frames.internal.emvar(obj.df.data,obj.alpha);
         end
+
     end
 end
 
@@ -31,14 +32,14 @@ function alpha = exponentialWeight(type,value)
     switch type
         case 'Alpha'
             alpha = value;
-        case 'Window'
+        case 'Com'  % center of mass
+            alpha = 1./(value+1);
+        case 'Window'  % similar to the SMA window, see https://en.m.wikipedia.org/wiki/Moving_average#Relationship_between_SMA_and_EMA
             alpha = 2./(value+1);
         case 'Span'
             alpha = 2./(value+1);
         case 'Halflife'
             alpha = 1-exp(log(0.5)./value);
-        case 'Com'
-            alpha = 1./(value+1);
         otherwise
             error('Type is unknown!')
     end
