2238bb34b149857cdce684eedd4937e11e7da00b
benjamingaudin <benjamin.gaudin@hotmail.com>
2021-08-25 19:30:50 +0200

    move to internal

--- +frames/private/isFrame.m
+++ +frames/private/isFrame.m
@@ -1,6 +1,3 @@
 function bool = isFrame(varargin)
-bool = false(1,nargin);
-for ii = 1:nargin
-    bool(ii) = isa(varargin{ii},'frames.DataFrame');
-end
+bool = frames.internal.isFrame(varargin{:});
 end
