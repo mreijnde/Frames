8ec9c8fbf73caf19c146a68bd4f71019e836dc19
benjamingaudin <benjamin.gaudin@hotmail.com>
2021-09-15 21:59:07 +0200

    test df asgn fails

--- +frames/+unitTests/dataframeTest.m
+++ +frames/+unitTests/dataframeTest.m
@@ -302,6 +302,8 @@ classdef dataframeTest < matlab.unittest.TestCase
             df = df.setIndexType('sorted');
             df.row(2) = df.row(1);
             t.verifyEqual(df,frames.DataFrame([5 2 3 2; 5 2 3 2; 4 4 4 4; 1 0 1 0],frames.Index(1:4,UniqueSorted=true,Name="Row"),["Var1","Var2","Var3","newCol"]))
+            t.verifyError(@isnotseries,'frames:elementWiseHandler:differentIndex')
+            function isnotseries, df.row(1) = df(2); end
         end
         
         function subsasgnWithDFTest(t)
