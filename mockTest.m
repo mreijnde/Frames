e3a4a2835a3a0d239d051ef4f42409e9c367d9d8
Benjamin Gaudin <57812158+benjamingaudin@users.noreply.github.com>
2021-10-03 13:45:17 +0200

    Create mockTest.m

---
+++ mockTest.m
@@ -0,0 +1,24 @@
+
+classdef mockTest < matlab.unittest.TestCase
+    
+    properties
+
+    end
+    
+    methods(Test)
+        function basicTest(t)
+            df1 = frames.DataFrame([10 50 100 150]',[1 5 10 15]).setRowsType('sorted');
+            df2 = frames.DataFrame([20 50 90 150]',[2 5 9 15]).setRowsType('sorted');
+            [df1a,df2a] = frames.align(df1,df2);
+            t.verifyEqual(df1a.data,[10 NaN 50 NaN 100 150]');
+            t.verifyEqual(df1a.rows,[1 2 5 9 10 15]');
+            t.verifyEqual(df2a.data,[NaN 20 50 90 NaN 150]');
+            t.verifyEqual(df2a.rows,[1 2 5 9 10 15]');
+            
+            df1aSingleOutput = frames.align(df1,df2,df1,df1,df2);
+            t.verifyEqual(df1a,df1aSingleOutput);
+t.verifyEqual(1,2)
+        end
+        
+    end
+end
