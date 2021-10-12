fd2425dd3b2101bb25b51b47ab36cf85a7013296
Benjamin Gaudin <57812158+benjamingaudin@users.noreply.github.com>
2021-10-12 23:11:43 +0200

    Update validVariableName.m

--- +frames/private/validVariableName.m
+++ +frames/private/validVariableName.m
@@ -3,7 +3,7 @@ str = string(str);
 str = str.replace(' ','_');
 str(ismissing(str)) = "";
 str = matlab.lang.makeValidName(str,ReplacementStyle='underscore');
-if ~(nargin==2) && ~allowRepeating
+if nargin==1 || ~allowRepeating
     str = matlab.lang.makeUniqueStrings(str,{},namelengthmax());
 end
 end
