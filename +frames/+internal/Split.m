0d827289f7a05c45481c375d072186b368349aa0
Benjamin Gaudin <57812158+benjamingaudin@users.noreply.github.com>
2021-10-13 20:37:55 +0200

    Update Split.m

--- +frames/+internal/Split.m
+++ +frames/+internal/Split.m
@@ -1,6 +1,6 @@
 classdef Split < dynamicprops
  % SPLIT split a Frame into groups to apply a function separately group by group
- % Use: dfsplit = df.split(frames.Groups).<apply,aggregate>(func[,args,flag])
+ % Use: dfsplit = df.split(frames.Groups[,flags]).<apply,aggregate>(func[,args,flag])
  %
  % ----------------
  % Parameters:
