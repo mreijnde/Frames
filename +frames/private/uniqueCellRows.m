8a827caaee6e17cac0f5b1f750f0797e95e7492e
Merijn Reijnders <19893132+mreijnde@users.noreply.github.com>
2021-11-03 22:59:09 +0100

    uniqueCellRows: add order option

--- +frames/private/uniqueCellRows.m
+++ +frames/private/uniqueCellRows.m
@@ -1,7 +1,9 @@
-function [rows_uniq, rows_uniqInd, val_uniq, val_ind] = uniqueCellRows(data)
+function [rows_uniq, rows_uniqInd, val_uniq, val_ind] = uniqueCellRows(data,order)
 % get unique row values of cell array
 % 
-% input: data cell array(Nrows,Ncols)
+% input: 
+%  - data: cell array(Nrows,Ncols)
+%  - order: string enum ('stable', 'sorted'), defines numbering order of indices values
 %
 % output:
 %  - rows_uniq:   cell array(Nrows_unique, Ncols) with unique row values
@@ -9,7 +11,7 @@ function [rows_uniq, rows_uniqInd, val_uniq, val_ind] = uniqueCellRows(data)
 %  - val_uniq:    cell (1,Ncols) with array of unique values per column
 %  - val_ind:     array(Nrows,Ncols) with index to unique values per column
 %
-
+if nargin<2, order="stable"; end
 % check input
 assert( iscell(data), "needs to be cell array");
 % get unique values per dimension
@@ -17,10 +19,10 @@ assert( iscell(data), "needs to be cell array");
 val_uniq  = cell(1,Ncols);
 val_ind   = zeros(Nrows,Ncols);
 for i=1:Ncols
-    [val_uniq{i}, ~, val_ind(:,i)] = unique([data{:,i}],'stable');
+    [val_uniq{i}, ~, val_ind(:,i)] = unique([data{:,i}],order);
 end
 % get unique rows index
-[rows_valind, ~, rows_uniqInd] = unique(val_ind,'rows','stable');
+[rows_valind, ~, rows_uniqInd] = unique(val_ind,'rows',order);
 % get unique row values as cell(Nrow,Ncol)
 Nrows_uniq = size(rows_valind,1);
 rows_uniq  = cell(Nrows_uniq ,Ncols);
