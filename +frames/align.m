c6725f7fac3ee64ef8c5b0cd1061d064d1441584
Merijn Reijnders <19893132+mreijnde@users.noreply.github.com>
2022-05-12 23:45:49 +0200

    align: make wrapper to DataFrame.alignDFs()

--- +frames/align.m
+++ +frames/align.m
@@ -1,16 +1,7 @@
-function varargout = align(dfs)
+function varargout = align(varargin)
 %ALIGN align frames to each others and ouputs each of them aligned
-% ToDo align columns flag, revisit this with tools from Merijn
-arguments(Repeating)
-    dfs frames.DataFrame
-end
-warning('off','frames:Index:notUnique')
-concatenate = [dfs{:}];  % align
-warning('on','frames:Index:notUnique')
-colID = 1;
-for ii=1:nargin  % split into varargout
-    nbCols = size(dfs{ii}.data_,2);
-    varargout{ii} = concatenate.iloc_(':',colID:colID+nbCols-1); %#ok<AGROW>
-    colID=colID+nbCols;
-end
+%
+% wrapper for DataFrame.alignDFs(), see there for more information
+varargout= cell(1,nargout);
+[varargout{:}] = alignDFs(varargin{:});
 
